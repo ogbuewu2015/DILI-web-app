5b933f67fdac701ef2c8ec2781d7eff1690d2cb2
ogbuewu2015 <ogbuewu2015@yahoo.com>
2026-06-17 11:10:58 -0600

    Load calibrated CAR model and print feature names

--- livertox_app_new.py
+++ livertox_app_new.py
@@ -802,6 +802,12 @@ if one_or_few_SMILES != "['CCO']":
 
         st.success("✅ CAR antagonist cytotoxic features selected.")
 
+        car_model = joblib.load("calibrated_car_model.pkl")
+
+        print(car_model.feature_names_in_)
+
+        print(cytotoxic_car)
+
         # ====================================================
         # STEP 15: MODEL PROBABILITIES
         # ====================================================
